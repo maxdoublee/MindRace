258d7c516b2c66027f5f94d60b93bc5f1f5a9f99
Max <105129935+maxdoublee@users.noreply.github.com>
2023-02-09 23:56:28 -0500

    Create PreLoadLEDs.ino

---
+++ code/Car/PreLoadLEDs.ino
@@ -0,0 +1,14 @@
+void PreLoadLEDs(){
+  digitalWrite(RIGHTREDLED, HIGH);
+  delay(500);
+  digitalWrite(RIGHTREDLED, LOW);
+  digitalWrite(LEFTGREENLED, HIGH);
+  delay(500);
+  digitalWrite(LEFTGREENLED, LOW);
+  digitalWrite(LEFTREDLED, HIGH);
+  delay(500);
+  digitalWrite(LEFTREDLED, LOW);
+  digitalWrite(RIGHTGREENLED, HIGH);
+  delay(500);
+  digitalWrite(RIGHTGREENLED, LOW);
+}
